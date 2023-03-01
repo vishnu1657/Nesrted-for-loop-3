e364b0b0869242a2fe7ca5104ed60c3d8fdf0890
HP <HP@DESKTOP-4Q8V1JF>
2023-03-01 17:16:27 +0530

    Add project files.

---
+++ Nesrted for loop 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Schema;
+
+public class program
+{
+    public static void Main()
+    {
+        pattern1();
+        pattern2();
+            
+        static void pattern1()
+        {
+            for(int i =1; i <= 10; i++) // rows
+            {
+                for (int j = 1; j <= 10; j++) // coloum
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void pattern2()
+        {
+            for(int i = 1; i<= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
