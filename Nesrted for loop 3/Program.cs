fb272cf948719ec9615a475c8d95a2b888ceeffc
HP <HP@DESKTOP-4Q8V1JF>
2023-03-01 17:20:38 +0530

    Nested for loop3

--- Nesrted for loop 3/Program.cs
+++ Nesrted for loop 3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Schema;
+﻿using System.Security.Cryptography;
+using System.Xml.Schema;
 
 public class program
 {
@@ -6,7 +7,8 @@ public class program
     {
         pattern1();
         pattern2();
-            
+        pattern3();
+        
         static void pattern1()
         {
             for(int i =1; i <= 10; i++) // rows
@@ -26,6 +28,17 @@ public class program
                 for (int j = 1; j <= 10; j++)
                 {
                     Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void pattern3()
+        {
+            for(int i =1; i <= 20; i++)
+            {
+                for (int j = 1; j <= 20; j++)
+                {
+                    Console.Write(j);
                 }
                 Console.WriteLine();
             }
