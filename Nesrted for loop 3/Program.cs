4004c982414497a643680dd08063b69524c5b94d
HP <HP@DESKTOP-4Q8V1JF>
2023-03-02 13:40:03 +0530

    Nested for loop 3

--- Nesrted for loop 3/Program.cs
+++ Nesrted for loop 3/Program.cs
@@ -8,7 +8,7 @@ public class program
         pattern1();
         pattern2();
         pattern3();
-        
+        pattern4();
         static void pattern1()
         {
             for(int i =1; i <= 10; i++) // rows
@@ -36,14 +36,25 @@ public class program
         {
             for(int i =1; i <= 20; i++)
             {
-                for (int j = 1; j <= 20; j++)
+                for (int j = 1; j <= 10; j++)
                 {
                     Console.Write(j);
                 }
                 Console.WriteLine();
             }
         }
+        static void pattern4()
+        {
+            for (int i=1; i <= 10; i++)
+            {
+                for (int j= 1; j<=i; j++)
+                {
+                    Console.Write(" *");
 
+                }
+                Console.WriteLine();
+            }
+        }
 
     }
 }
